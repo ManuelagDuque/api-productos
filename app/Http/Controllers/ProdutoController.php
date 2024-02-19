b782d2bd0f15ce6f7e4aa99163d62f1be0df5f3d
ManuelagDuque <grajalesduquemanuela@gmail.com>
2024-02-19 09:47:51 +0100

    ProductController methods done

---
+++ app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Producto;
+use Illuminate\Http\Request;
+
+class ProdutoController extends Controller
+{
+    public function index(){
+        $productos = Producto::all();
+        return $productos;
+    }
+
+    public function store(Request $request){
+        $producto = new Producto;
+        $producto -> nombre = $request -> nombre;
+        $producto -> precio = $request -> precio;
+
+        $producto -> save();
+        return $producto;
+    }
+
+    public function show($id){
+        $producto = Producto::find($id);
+        return $producto;
+    }
+
+    public function update(Request $request, $id){
+        $producto = Producto::findOrFail($request->id);
+        $producto -> nombre = $request -> nombre;
+        $producto -> precio = $request -> precio;
+
+        $producto -> save();
+        return $producto;
+    }
+
+    public function destroy($id){
+        $producto = Producto::destroy($id);
+        return $producto;
+    }
+
+    
+
+}
