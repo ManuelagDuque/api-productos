2e1caa2e6fdc608b1402d600cc6e1f85cfc29345
ManuelagDuque <grajalesduquemanuela@gmail.com>
2024-02-19 10:00:25 +0100

    routes done

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Producto;
 use Illuminate\Http\Request;
 
-class ProdutoController extends Controller
+class ProductoController extends Controller
 {
     public function index(){
         $productos = Producto::all();
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::controller(ProductoController::class)->group(function(){
+    Route::get('/productos', 'index');
+    Route::post('/producto', 'store');
+    Route::get('/producto/{id}', 'show');
+    Route::put('/producto/{id}', 'update');
+    Route::delete('/producto/{id}', 'destroy');
 });
